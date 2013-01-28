efd313bd11d272aa49e401f3812d0ec8a5e8849f
colourblind <tom.milsom@gmail.com>
2013-01-28 22:59:25 +0000

    Jinja formatter for slugging. Homepage generation

--- psv.py
+++ psv.py
@@ -26,10 +26,15 @@ def slugify(name):
 def format_datetime(value):
     return value.strftime('%d/%m/%Y %H:%M')
     
+def format_slug(value):
+    return slugify(value)
+    
 def go(config): 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('.', encoding='utf-8'))
     env.filters['datetime'] = format_datetime
+    env.filters['slug'] = format_slug
     template = env.get_template(config['template'])
+    index_template = env.get_template(config['index_template']);
     if not os.path.exists(config['out_path']):
         os.makedirs(config['out_path'])
     # TODO: pick loaders dynamically
@@ -38,10 +43,16 @@ def go(config):
         row.body = modifiers.tweak(row.body)
         markup = template.render(data=row)
         filename = '{0}/{1}.html'.format(config['out_path'], slugify(row.title))
+        print('<add from="/post/{0}.aspx" to="/{1}.html" />'.format(slugify(row.title).lower(), slugify(row.title)))
         f = open(filename, 'w')
         f.write(markup)
         f.close()
-
+        
+    # Create homepage
+    f = open('{0}/index.html'.format(config['out_path']), 'w')
+    f.write(index_template.render(data=data))
+    f.close()
+    
 if __name__ == '__main__':
     config = load_config(sys.argv[1])
     go(config)
